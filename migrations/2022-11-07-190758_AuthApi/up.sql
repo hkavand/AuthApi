2520409454328ed0a9cc553b3c0dff3969287b97
hossein kavand <kavand.hossain@gmail.com>
2022-11-10 16:35:05 +0330

    remove extra migration

--- migrations/2022-11-07-190758_AuthApi/up.sql
+++ migrations/2022-11-07-190758_AuthApi/up.sql
@@ -7,11 +7,3 @@ CREATE TABLE users (
     fullName VARCHAR(64) NOT NULL,
     phonenNumber VARCHAR(16) NOT NULL
 );
-
-CREATE TABLE user_tokens (
-    id SERIAL PRIMARY Key NOT NULL,
-    userid SERIAL NOT NULL,
-    token VARCHAR(128) NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    expiresAt TIMESTAMP NOT NULL
-);
